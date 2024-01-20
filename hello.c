b2f17b7203a0121de3952d99112344747aeac148
cpiau72 <claudine.piau@gmail.com>
2024-01-20 16:15:26 +0100

    validation

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@
 //
 //  Created by Claudine Piau Toffolon on 24/02/2021.
 //  Copyright © 2021 Claudine Piau Toffolon. All rights reserved.
-//
+// modification du 20/01/2024 " Bonne année 2024"
 
 
 #include <stdio.h>
@@ -14,5 +14,5 @@ int main ()
     /* afficher le mot bonjour et saut de ligne */
     printf("Hello world , It's  Claudine 's new version!\n");
     return 2;
-    
+
 }
