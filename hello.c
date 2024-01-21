bc11f29fa5ba4f771c44f80b2b6252392b33a9ab
cpiau <claudine.piau@gmail.com>
2024-01-21 17:09:25 +0100

    modif dans master

--- hello.c
+++ hello.c
@@ -5,6 +5,7 @@
 //  Created by Claudine Piau Toffolon on 24/02/2021.
 //  Copyright © 2021 Claudine Piau Toffolon. All rights reserved.
 // modification du 20/01/2024 " Bonne année 2024"
+// nouvelle modif dans branche master
 
 #include <stdio.h>
 #include <stdlib.h>
