a23530415f93b86c679c9e2e881a36f97bc79137
cpiau <claudine.piau@gmail.com>
2024-01-21 17:17:19 +0100

    modif hello.c

--- hello.c
+++ hello.c
@@ -1,22 +1,18 @@
 //
 //  hello.c
-//  
+//
 //
 //  Created by Claudine Piau Toffolon on 24/02/2021.
 //  Copyright © 2021 Claudine Piau Toffolon. All rights reserved.
 // modification du 20/01/2024 " Bonne année 2024"
-<<<<<<< HEAD
-// nouvelle modif dans branche master
-=======
-// modif dans brabche testing 
->>>>>>> testing
+// modif dans branche testing
 
 #include <stdio.h>
 #include <stdlib.h>
 int main ()
 {
     /* afficher le mot bonjour et saut de ligne */
-    printf("Hello world , It's  me again  !\n");
+    printf("Hello world , It's  me   !\n");
     return 2;
 
 }
