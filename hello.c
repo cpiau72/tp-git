09b8e97e47b056aa9183535a8ad8d58e1d180518
cpiau <claudine.piau@gmail.com>
2024-01-21 16:34:31 +0100

    v2

--- hello.c
+++ hello.c
@@ -5,9 +5,6 @@
 //  Created by Claudine Piau Toffolon on 24/02/2021.
 //  Copyright © 2021 Claudine Piau Toffolon. All rights reserved.
 // modification du 20/01/2024 " Bonne année 2024"
-// modif 2
-//modif 3
-//modif 4
 
 #include <stdio.h>
 #include <stdlib.h>
