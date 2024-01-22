e515041eacef1db91a5a284112d6b9247aefc2ec
cpiau <claudine.piau@gmail.com>
2024-01-22 14:08:33 +0100

    valid 1

---
+++ f1.c
@@ -0,0 +1,17 @@
+//
+//  hello.c
+//
+//
+//  Created by Claudine Piau Toffolon on 24/02/2021.
+//  Copyright © 2021 Claudine Piau Toffolon. All rights reserved.
+// modification du 20/01/2024 " Bonne année 2024"
+ 
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+    /* afficher le mot bonjour et saut de ligne */
+    printf("Hello world , It's  me   !\n");
+    return 2;
+
+}
